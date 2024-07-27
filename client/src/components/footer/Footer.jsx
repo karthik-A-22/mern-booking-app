26830a298d38abe8f1996d50b9035fea781a540b
karthik-A-22 <karthik.a2219905@gmail.com>
2024-07-27 17:16:52 +0530

    "Updated Featured component with new city images and data, updated Footer component with new copyright year and styling, and refactored Hotel controller with consistent spacing and formatting."

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
           <li className="fListItem">Terms & conditions</li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2022 booking app.</div>
+      <div className="fText">Copyright © 2024 booking app.</div>
     </div>
   );
 };
